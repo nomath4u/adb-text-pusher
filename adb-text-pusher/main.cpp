ba0f3470d631a24d176eaaab058f5ef80699e381
nomath4u <nomath4u@gmail.com>
2013-11-19 20:59:33 -0800

    Refactored GUI setup into its own function

--- adb-text-pusher/main.cpp
+++ adb-text-pusher/main.cpp
@@ -3,19 +3,41 @@
 #include <QtGui>
 #include <QPushButton>
 #include <QVBoxLayout>
+
+QWidget* setUpGUI();
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     //MainWindow w;
-    QPushButton* quitButton = new QPushButton("&Quit");
+    /*QPushButton* quitButton = new QPushButton("&Quit");
     QObject::connect(quitButton, SIGNAL(clicked()), qApp, SLOT(quit()));
     QVBoxLayout *layout = new QVBoxLayout;
     layout->addWidget(quitButton);
     //w.setLayout(layout);
 
     QWidget window;
-    window.setLayout(layout);
-    window.show();
+    window.setLayout(layout);*/
+    QWidget* window = setUpGUI();
+    window->show();
 
     return a.exec();
+}
+
+/*Setup GUI*/
+QWidget* setUpGUI(){
+    QWidget* window = new QWidget();
+
+     /*Create Widgets*/
+     QPushButton* quitButton = new QPushButton("&Quit");
+     QObject::connect(quitButton, SIGNAL(clicked()), qApp, SLOT(quit()));
+
+     /*Define the layout*/
+     QVBoxLayout *layout = new QVBoxLayout;
+     layout->addWidget(quitButton);
+
+     window->setLayout(layout);
+
+     return window;
+
 }
