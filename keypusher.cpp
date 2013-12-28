2cc1f5e6371fe77be52a0b04d96436dc6d242cae
Chris <harperc@onid.orst.edu>
2013-12-27 17:48:08 -0800

    Moved keypusher prototypes to a header and fixed the git ignore for both old things that should not be ignored and to ignore the new release folder

--- keypusher.cpp
+++ keypusher.cpp
@@ -4,20 +4,11 @@
 #include <vector>
 #include <sstream>
 #include <sys/time.h>
+#include "keypusher.h"
 
 using namespace std;
 
-void send_string(vector<string>);
-vector<string> parse_string(string);
-bool check_apostrophe(string&);
-void send_apostrophe_string(vector<string>);
-void replace_apostrophe(string&);
-void get_sms();
-void take_photo();
-void parse_Flags(int, char**);
-void push_Script();
 
-bool textmode;
 
 
 void send_string(vector<string> words){
--- keypusher.h
+++ keypusher.h
@@ -1,4 +1,18 @@
 #ifndef KEYPUSHER_H
 #define KEYPUSHER_H
 
+using namespace std;
+
+void send_string(vector<string>);
+vector<string> parse_string(string);
+bool check_apostrophe(string&);
+void send_apostrophe_string(vector<string>);
+void replace_apostrophe(string&);
+void get_sms();
+void take_photo();
+void parse_Flags(int, char**);
+void push_Script();
+
+bool textmode;
+
 #endif // KEYPUSHER_H
